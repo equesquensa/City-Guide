2db6cc61512624bb29406563632efae068c0582b
budget2019 <equesquensa@gmail.com>
2020-03-10 13:01:22 -0500

    City tours

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,9 @@ function Navbar() {
     <img src={logo} alt="logo" style={{width:'60px'}}/>
     </Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <span>
+      <i className="fas fa-bars" style={{color: '#fff'}} />
+    </span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
--- src/src/data.js
+++ src/src/data.js
@@ -10,9 +10,7 @@ export const placeInfo = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates qui voluptate odio quod doloremque unde reprehenderit velit quaerat. Deserunt voluptatem natus suscipit aliquid, eligendi sequi dicta porro? Laudantium, libero est?',
     img: '.././images/garaj.jpg',
     maps:
-      /*'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'*/
-
-      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.5990899915946!2d-77.06881688578848!3d-12.002218844339774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cf7cd9d0b8d3%3A0x71c77211e1c9c4ce!2sLa%20bombonera!5e0!3m2!1sen!2spe!4v1583778711318!5m2!1sen!2spe'
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
   },
   {
     id: 2,
